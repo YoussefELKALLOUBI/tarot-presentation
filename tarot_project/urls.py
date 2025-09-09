90c3065aa14f8e9d9489b99b1de731560efe69d4
El-Kalloubi Youssef <youssef.el-kalloubi@etu.univ-orleans.fr>
2025-09-09 23:47:36 +0200

    Fix navigation between pages

--- tarot_project/urls.py
+++ tarot_project/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls import handler404
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('presentation.urls')),
 ]
+
+if not settings.DEBUG:
+    handler404 = 'presentation.views.custom_404_view'
